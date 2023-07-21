79bf489ce803cf14b8c709a8afec5bc7f64d5b9c
BSY <45937750+ideawolf@users.noreply.github.com>
2023-07-21 15:23:59 +0900

    fix style

--- src/pages/midilist/components/UploadPopup.js
+++ src/pages/midilist/components/UploadPopup.js
@@ -56,6 +56,30 @@ const UploadPopup = ({ onClose }) => {
             isOpen={true}
             onRequestClose={handleCancel}
             style={{
+                overlay: {
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundColor: 'rgba(15, 15, 15, 0.79)',
+                },
+                content: {
+                    position: 'absolute',
+                    top: '60px',
+                    left: '35%',
+                    width: '30%',
+                    height: '80%',
+                    border: '1px solid #ccc',
+                    background: '#fff',
+                    overflow: 'auto',
+                    WebkitOverflowScrolling: 'touch',
+                    borderRadius: '4px',
+                    outline: 'none',
+                    padding: '20px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                },
                 // 스타일은 동일하게 유지
             }}
         >
