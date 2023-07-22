4d28170d17c669ee19ab0fb7ffdcbc6de738bd67
ahyoung <ahyoung120613@gmail.com>
2023-07-22 12:51:33 +0900

    Routes.js 오류 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import NotePlayPage from "./pages/NotePlay/NotePlayPage"
-import MidiListPage from "./pages/MidiList/MidiListPage"
+import MidiListPage from "./pages/midilist/MidiListPage"
 import MidiPlayPage from "./pages/MidiPlay/MidiPlayPage"
 import { Routes, Route } from "react-router-dom"
 
