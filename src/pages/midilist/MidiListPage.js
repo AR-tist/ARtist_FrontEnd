9ed8e64e8e8f48fe6a20311c7859abd7f8881262
ah-o-ng12 <ahyoung120613@gmail.com>
2023-09-04 20:48:19 +0900

    menu component

--- src/pages/midilist/MidiListPage.js
+++ src/pages/midilist/MidiListPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Song from "./components/Song";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Menu from "./Menu";
 import UploadPopup from "./components/UploadPopup";
 import "./MidiListPage.css";
 import { useDispatch, useSelector } from "react-redux";
@@ -178,120 +179,8 @@ const MidiListPage = () => {
       </div>
 
       <Header toggleSidebar={toggleSidebar} />
-      {/* 사이드바 */}
-      {isSidebarOpen && (
-        <div
-          className="sidebar"
-          style={{
-            position: "fixed",
-            justifyContent: "center",
-            top: 0,
-            right: 0,
-            width: "240px",
-            height: "100%",
-            backgroundColor: "#f5f5f5",
-            boxShadow: "-2px 0 2px rgba(0, 0, 0, 0.2)",
-          }}
-        >
-          {/* 프로필 이미지 */}
-          <div
-            className="profile-info"
-            style={{
-              marginTop: "120px",
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <div className="profile-image" style={{ marginLeft: "20px" }}>
-              <img
-                className="Profile-img"
-                src="img\프로필.png"
-                alt="프로필 이미지"
-                width="34"
-                height="34"
-              />
-            </div>
 
-            {/* 닉네임 */}
-            <h4 style={{ marginLeft: "20px" }}>닉네임</h4>
-          </div>
-
-          {/* 가로선 추가 */}
-          <hr
-            style={{
-              width: "90%",
-              borderTop: "1px solid #ddd",
-              margin: "20px auto",
-            }}
-          />
-
-          <div
-            className="menu-list"
-            style={{
-              margin: "300px 20px",
-            }}
-          >
-            {/* '방 만들기' 버튼 */}
-            <div className="list">
-              <img
-                className="room-img"
-                src="img\방만들기.png"
-                alt="방만들기 이미지"
-                width="24"
-                height="28"
-                style={{ marginLeft: "3px" }}
-              />
-
-              <button
-                className="sidebar-button"
-                style={{
-                  backgroundColor: "#fff",
-                  border: "1px solid #ddd",
-                  borderRadius: "5px",
-                  cursor: "pointer",
-                  display: "block",
-                  backgroundColor: "transparent",
-                  border: "none",
-                  boxSahdow: "none",
-                  fontSize: "15px",
-                  marginLeft: "22px",
-                }}
-              >
-                방 만들기
-              </button>
-            </div>
-
-            <div className="list">
-              {/* '파일 변환하기' 버튼 */}
-              <img
-                className="conversion-img"
-                src="img\변환.png"
-                alt="변환 이미지"
-                width="30"
-                height="30"
-              />
-              <button
-                className="sidebar-button"
-                style={{
-                  backgroundColor: "#fff",
-                  border: "1px solid #ddd",
-                  borderRadius: "5px",
-                  cursor: "pointer",
-                  display: "block",
-                  backgroundColor: "transparent",
-                  border: "none",
-                  boxSahdow: "none",
-                  fontSize: "15px",
-                  marginLeft: "20px",
-                }}
-              >
-                파일 변환하기
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+      <Menu isSidebarOpen={isSidebarOpen} />
     </>
   );
 };
