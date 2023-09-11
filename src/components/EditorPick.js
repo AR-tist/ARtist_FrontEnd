8ad45c220dc24b4d3c730f33cb0420ad676b8dce
Min-gyu Jeong <jkg7170@gachon.ac.kr>
2023-09-11 19:55:14 +0900

    update layout

--- src/components/EditorPick.js
+++ src/components/EditorPick.js
@@ -1,28 +1,28 @@
 import React from "react";
 
 function EditorPickTile({ imageUrl, title }) {
-    return (
-      <div style={{ display: 'flex', flexDirection: 'column'}}>
-        <div style={{ width: '175px', marginBottom: '10px', marginLeft: '20px' }}>
-        <img src={imageUrl} alt={title} style={{  }} />
-        <p style={{marginTop: '10px'}}>{title}</p>
-        </div>
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ width: '175px', marginBottom: '10px', marginLeft: '20px' }}>
+        <img src={imageUrl} alt={title} style={{}} />
+        <p style={{ marginTop: '10px', fontSize: "14px", color: "#333" }}>{title}</p>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
+
 
 const EditorPick = ({ items }) => {
   return (
-    <div>
-      <h2 style={{marginBottom: '20px'}}>Editor's Pick</h2>
+    <div style={{ marginLeft: '10px', marginBottom: "10px" }}>
+      <h2 style={{ marginBottom: '20px' }}>Editor's Pick</h2>
       <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginLeft: '-20px' }}>
         {items.map((item, index) => (
           <EditorPickTile key={index} imageUrl={item.imageUrl} title={item.title} />
         ))}
       </div>
     </div>
-  );    
+  );
 }
 
 export default EditorPick
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 const Layout = ({ children }) => {
-    return <div style={{ paddingLeft: "285px" }}>
+    return <div style={{ paddingTop: "30px", paddingLeft: "285px" }}>
         <div style={{ margin: "0 43px" }}>
             {children}
         </div>
