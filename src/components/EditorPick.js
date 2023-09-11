e29827efc3dd5aa3df6fb280c04d2f1ef99483ac
BSY <45937750+ideawolf@users.noreply.github.com>
2023-09-11 19:44:03 +0900

    Editor's pick

---
+++ src/components/EditorPick.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function EditorPickTile({ imageUrl, title }) {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column'}}>
+        <div style={{ width: '175px', marginBottom: '10px', marginLeft: '20px' }}>
+        <img src={imageUrl} alt={title} style={{  }} />
+        <p style={{marginTop: '10px'}}>{title}</p>
+        </div>
+      </div>
+    );
+  }
+  
+
+const EditorPick = ({ items }) => {
+  return (
+    <div>
+      <h2 style={{marginBottom: '20px'}}>Editor's Pick</h2>
+      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginLeft: '-20px' }}>
+        {items.map((item, index) => (
+          <EditorPickTile key={index} imageUrl={item.imageUrl} title={item.title} />
+        ))}
+      </div>
+    </div>
+  );    
+}
+
+export default EditorPick
