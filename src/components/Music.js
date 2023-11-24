be4877ea378fd5290541f75e8a6a8108509d76d8
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-11-24 17:14:45 +0900

    Create class

---
+++ src/components/Music.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Music {
+    constructor(music_name, ){
+        this.music_name = music_name;
+    }
+}
+export default Music;
---
+++ src/components/Room.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Client from "./Client";
+import Music from "./Music";
+
+class Room {
+    constructor(room_id, host_id) {
+        this.room_id = room_id || null;
+        this.host_id = host_id || null;
+        this.music_instance = new Music();
+        this.invite_url = null;
+        this.guests = {};
+    }
+
+    getRoom_id() {
+        return this.room_id;
+    }
+
+    getHost_id() {
+        return this.host_id;
+    }
+
+    getMusic_instance() {
+        return this.music_instance;
+    }
+
+    getInvite_url() {
+        return this.invite_url;
+    }
+
+    getGuests() {
+        return this.guests;
+    }
+}
+
+export default Room;
