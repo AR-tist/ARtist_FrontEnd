32fef224c3014c25aafa14f9cbe22b00b358752f
ah-o-ng12 <ahyoung12@gachon.ac.kr>
2023-12-02 13:59:45 +0900

    Udate editorpicks

--- src/pages/MidiList/MidiListPage.js
+++ src/pages/MidiList/MidiListPage.js
@@ -37,15 +37,42 @@ const MidiListPage = () => {
     setSidebarOpen(!isSidebarOpen);
   };
 
-
   const editorpicks = [
-    { imageUrl: 'https://cdn.music-flo.com/poc/p/image/channel/rep/20221222/64c75b6c7c204806a43dba60588edca6.png/dims/resize/175/quality/90', title: '가을에 듣기 좋은 발라드 Top 40' },
-    { imageUrl: 'https://cdn.music-flo.com/poc/p/image/channel/rep/20230904/8bb630f1161346399c3a50abc39d78a2.jpg/dims/resize/175/quality/90', title: '인스타에서 듣고 바로 반해버린 팝💘' },
-    { imageUrl: 'https://cdn.music-flo.com/poc/p/image/channel/rep/20221222/64c75b6c7c204806a43dba60588edca6.png/dims/resize/175/quality/90', title: '가을에 듣기 좋은 발라드 Top 40' },
-    { imageUrl: 'https://cdn.music-flo.com/poc/p/image/channel/rep/20230904/8bb630f1161346399c3a50abc39d78a2.jpg/dims/resize/175/quality/90', title: '인스타에서 듣고 바로 반해버린 팝💘' },
-    { imageUrl: 'https://cdn.music-flo.com/poc/p/image/channel/rep/20221222/64c75b6c7c204806a43dba60588edca6.png/dims/resize/175/quality/90', title: '가을에 듣기 좋은 발라드 Top 40' },
-    { imageUrl: 'https://cdn.music-flo.com/poc/p/image/channel/rep/20230904/8bb630f1161346399c3a50abc39d78a2.jpg/dims/resize/175/quality/90', title: '인스타에서 듣고 바로 반해버린 팝💘' },
-    { imageUrl: 'https://cdn.music-flo.com/poc/p/image/channel/rep/20221222/64c75b6c7c204806a43dba60588edca6.png/dims/resize/175/quality/90', title: '가을에 듣기 좋은 발라드 Top 40' },
+    {
+      imageUrl:
+        "https://cdn.music-flo.com/image/album/931/286/02/04/402286931_5c9d73de.jpg?1553822687261/dims/resize/175/quality/90",
+      title: "너와 함께라면 추운 겨울도 견딜만해",
+    },
+    {
+      imageUrl:
+        "https://cdn.music-flo.com/image/v2/album/897/395/05/04/405395897_5fb69b00_o.jpg?1605802753351/dims/resize/175/quality/90",
+      title: "커피와 찰떡궁합 캐럴 팝 발라드",
+    },
+    {
+      imageUrl:
+        "https://cdn.music-flo.com/image/album/396/287/04/04/404287396_5df0adde.jpg?1576054238835/dims/resize/175/quality/90",
+      title: "올 크리스마스에틑 색다른 K-캐럴을",
+    },
+    {
+      imageUrl:
+        "https://cdn.music-flo.com/image/album/798/146/00/04/400146798_5cb1354e.jpg?1555117391586/dims/resize/175/quality/90",
+      title: "눈 오는 날 카페 창가에 앉아 듣는 노래",
+    },
+    {
+      imageUrl:
+        "https://cdn.music-flo.com/poc/p/image/channel/rep/20231127/2c5761818641436ba780d96b0fff50c8.png/dims/resize/175/quality/90",
+      title: "크리스마스 하면 떠오르는 영화 OST",
+    },
+    {
+      imageUrl:
+        "https://cdn.music-flo.com/poc/p/image/channel/rep/20230303/0d557e48d822482db706abb525cf6569.png/dims/resize/175/quality/90",
+      title: "런던의 LP바에 흐르는 올드 브릿팝",
+    },
+    {
+      imageUrl:
+        "https://cdn.music-flo.com/poc/p/image/channel/rep/20221222/64c75b6c7c204806a43dba60588edca6.png/dims/resize/175/quality/90",
+      title: "가을에 듣기 좋은 발라드 Top 40",
+    },
   ];
 
   return (
