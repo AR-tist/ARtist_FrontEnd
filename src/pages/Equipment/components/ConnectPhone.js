9ac8b0c60ffa3a4c22d23c3f3a0d8e1381ba7b9a
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-12-11 00:42:02 +0900

    Update ConnectPhone.js

--- src/pages/Equipment/components/ConnectPhone.js
+++ src/pages/Equipment/components/ConnectPhone.js
@@ -3,20 +3,37 @@ import { setPhoneWsbaseURL, getPhoneWsbaseURL } from "../../../utils/axios";
 
 const ConnectPhone = () => {
   const [serverStatus, setServerStatus] = useState("Stopped");
+  const [ws, setWs] = useState(null);
 
-  const Connecting = () => {
+  const connect = () => {
     const ip_address = document.getElementById("ip_address").value;
-    const ws = new WebSocket("ws://" + ip_address + ":4439");
-    console.log("ws", ws);
-
-    ws.onopen = () => {
+    const newWs = new WebSocket("ws://" + ip_address + ":4439");
+    setWs(newWs);
+  
+    newWs.onopen = () => {
       // connection opened
       console.log("connected");
-      ws.send("ready"); // send a message
+      newWs.send("ready"); // send a message
       setServerStatus("Running"); // Update server status in React state
     };
-
+  
+    newWs.onerror = (error) => {
+      console.error("WebSocket encountered an error:", error);
+      // Handle the error as needed
+      setServerStatus("Error"); // Update server status in React state
+    };
+  
     // Handle other WebSocket events here if needed (onmessage, onclose, etc.)
+  };
+  
+  const disconnect = () => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.close();
+      console.log("disconnecting");
+    } else {
+      console.log("WebSocket is not open or already closed.");
+    }
+    setServerStatus("Stopped"); // Update server status when disconnected
   };
 
   return (
@@ -51,7 +68,7 @@ const ConnectPhone = () => {
       />
       <button
         onClick={() => {
-          Connecting();
+          connect();
         }}
         style={{
           width: "300px",
@@ -66,6 +83,25 @@ const ConnectPhone = () => {
         }}
       >
         연결하기
+      </button>
+
+      <button
+        onClick={() => {
+          disconnect();
+        }}
+        style={{
+          width: "300px",
+          height: "50px",
+          position: "absolute",
+          left: "668px",
+          top: "792px", // Adjusted the top position
+          fontSize: "24px",
+          fontWeight: "500",
+          textAlign: "left",
+          color: "#000",
+        }}
+      >
+        연결 끊기
       </button>
 
       <div>
